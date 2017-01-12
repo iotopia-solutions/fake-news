d8afd8841969bc0dbd405dc45f8f0f7d8e1c8e96
John Hann <john@unscriptable.com>
2017-01-11 23:13:10 -0500

    Use creed shim for better Promises.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // This is the app bootstrapper.
+
+// Upgrade slow, buggy native promises
+require('creed').shim()
+
 // If we're in dev mode, run from src.
 process.env.NODE_ENV === 'dev'
     ? require('./src/main').main()
